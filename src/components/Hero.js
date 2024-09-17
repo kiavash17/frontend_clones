8a9dfe7391239f2b2bfa2bcbcb28562f06a29a2f
kiavash17 <39773527+kiavash17@users.noreply.github.com>
2024-09-17 11:34:04 -0400

    Add files via upload

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,15 @@
+
 import React from 'react';
 import './Hero.css';
 
 function Hero() {
   return (
     <section className="hero-section">
+      <div className="animated-background">
+        <div className="circle circle1"></div>
+        <div className="circle circle2"></div>
+        <div className="circle circle3"></div>
+      </div>
       <div className="hero-content">
         <h1 className="hero-title">AI Powered by Your Course Materials</h1>
         <p className="hero-description">Instant and accurate AI responses based on your class slides and readings.</p>
